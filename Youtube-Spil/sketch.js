2a1b88acc084bfddaced7a88243670cfe092ccf3
Jesper <jellelarsen90@gmail.com>
2020-01-09 10:25:33 +0100

    start

---
+++ Youtube-Spil/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+}
+
+function ship() {
+    this.pos = createVector(width/2, height/2)
+
+    this.render = function() {
+
+        
+    }
+}
