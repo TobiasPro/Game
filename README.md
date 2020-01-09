2a9a36fa852030962b03bc0331d753aa0c9bb68f
Tobias <tobiasbjerregaard@live.dk>
2020-01-09 08:44:49 +0100

    kopiret code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Game
+Tobias og Jesper spil
---
+++ Spil/sketch.js
@@ -0,0 +1,33 @@
+let x = 100;
+let y = 100;
+let z = 100
+
+function setup() {
+  createCanvas(1280, 720);
+}
+
+function draw() {
+  background(0);
+  strokeWeight(1);
+  stroke(255)
+  fill(0)
+  // head
+    if (keyIsDown(LEFT_ARROW)) {
+      x -= 5;
+    }
+  
+    if (keyIsDown(RIGHT_ARROW)) {
+      x += 5;
+    }
+  
+    if (keyIsDown(UP_ARROW)) {
+      y -= 5;
+    }
+  
+    if (keyIsDown(DOWN_ARROW)) {
+      y += 5;
+    }
+  
+    clear();
+    triangle(30, 75, 58, 20, 86, 75);;
+  }
